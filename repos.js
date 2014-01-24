d3fa22c3a4b64cc63efbd2ea00e7a54852e5bce1
boo1ean <boo1ean0807@gmail.com>
2014-01-24 14:00:23 +0200

    Add test data

---
+++ repos.js
@@ -0,0 +1,16 @@
+var big = [
+	'rails/rails',
+	'laravel/framework',
+	'jquery/jquery',
+	'jashkenas/backbone'
+];
+
+var small = [
+	'boo1ean/mgrt',
+	'boo1ean/mgrt-file-storage',
+	'boo1ean/mgrt-migrations',
+	'boo1ean/cnfg',
+	'boo1ean/dotfiles'
+];
+
+module.exports = small;
