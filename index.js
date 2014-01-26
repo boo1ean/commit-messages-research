cda48adce28afae36622bd21d71485d74d0078e5
boo1ean <boo1ean0807@gmail.com>
2014-01-26 19:39:24 +0200

    Update test example

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var repos = require('./repos'),
 
 var options = {
 	handler: function(commits, repo) {
-		console.log(repo, commits.length);
+		console.log(commits);
 	},
 	
 	map: function(entry) {
 		return entry.commit.message;
+	},
+
+	secrets: {
+		client_id: 'xxxxxxxxx',
+		client_secret: 'yyyyyyyyyy'
 	}
 };
 
