de4c57b10fa4857c5f25a61bfdfa280f42875d99
boo1ean <boo1ean0807@gmail.com>
2014-01-27 02:06:16 +0200

    Add commits filter

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ var repos = require('./repos'),
 var options = {
 	handler: function(commits, repo) {
 		var result = scan(commits, predicates);
-		console.log(repo, commits.length);
+
+		var sum = 0;
+		for (var i in commits) {
+			sum += commits[i].length;
+		}
+
+		result['Average commit message length'] = Math.floor(sum / commits.length);
+		result['Github repository'] = repo;
+		result['Total commits number'] = commits.length
 		console.log(result);
 	},
 	
 	map: function(entry) {
 		return entry.commit ? entry.commit.message : '';
+	},
+
+	filter: function(message) {
+		return Boolean(message)
+		&& message.slice(0, 12) != 'Merge branch'
+		&& message.slice(0, 18) != 'Merge pull request';
 	},
 
 	secrets: secrets
